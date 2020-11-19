4681fbb091b01da93f202273aecb02dace6b9eae
HabutoShogo <habuto.shogo@gmail.com>
2020-11-19 15:03:23 +0900

    fix

--- api/infrastructure/route.go
+++ api/infrastructure/route.go
@@ -1,19 +1,21 @@
-package route
+package infrastructure
 
 import (
-	"sample/handler"
-
+	"sample/interfaces/controllers"
 	"github.com/labstack/echo"
 )
 
-func Init() *echo.Echo {
+func Init() {
+	// Echo instance
 	e := echo.New()
 
-	// Routes
-	v1 := e.Group("/api/v1")
-	{
-		v1.GET("/handler", handler.Sample())
-	}
-	return e
+		// Routes
+		v1 := e.Group("/api/v1")
+		{
+			v1.GET("/sample", controllers.Sample())
+		}
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8000"))
 }
 
--- api/main.go
+++ api/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"sample/route"
+	"sample/infrastructure"
 
-	"github.com/sirupsen/logrus"
+	// "github.com/sirupsen/logrus"
 )
 
-func init() {
-	logrus.SetLevel(logrus.DebugLevel)
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-}
-
 func main() {
-	router := route.Init()
-	router.Start(":3000")
+	infrastructure.Init()
 }
